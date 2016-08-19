2a36fc605769271856587e6e9665335e12ac6cc3
ghilbing <ghilbing@gmail.com>
2016-08-19 16:31:42 -0700

    First Version

--- app/src/main/java/com/example/android/myappportfolio/MainActivity.java
+++ app/src/main/java/com/example/android/myappportfolio/MainActivity.java
@@ -3,30 +3,62 @@ package com.example.android.myappportfolio;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.SwipeDismissBehavior;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button1, button2, button3, button4, button5, button6;
+    String message;
+
+    public void showToast(View v){
+
+        switch (v.getId()){
+            case R.id.button1:
+                message = "This button will launch Popular Movies";
+                break;
+
+            case R.id.button2:
+                message = "This button will launch Stock Hawk";
+                break;
+
+            case R.id.button3:
+                message = "This button will launch Build it Bigger";
+                break;
+
+            case R.id.button4:
+                message = "This button will launch Make your App Material";
+                break;
+
+            case R.id.button5:
+                message = "This button will launch Go Obiquitous";
+                break;
+
+            case R.id.button6:
+                message = "This button will launch Capstone";
+                break;
+        }
+
+        Toast.makeText(getApplicationContext(),message,Toast.LENGTH_LONG).show();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
